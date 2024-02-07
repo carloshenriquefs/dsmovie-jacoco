478e991929f77cfc75a45c7a28041157e010c42b
Carlos Henrique <ichfs9108@gmail.com>
2024-02-07 16:20:53 -0300

    Project created

---
+++ dsmovie/dsmovie/src/test/java/com/dsmovie/jacoco/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmovie.jacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
