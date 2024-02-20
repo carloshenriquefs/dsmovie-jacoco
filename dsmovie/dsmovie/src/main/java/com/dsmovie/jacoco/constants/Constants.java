6fbad1cc2de45eb04cf83364703fb79f785fee0b
Carlos Henrique <ichfs9108@gmail.com>
2024-02-19 21:47:44 -0300

    Adicionando constants

---
+++ dsmovie/dsmovie/src/main/java/com/dsmovie/jacoco/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.dsmovie.jacoco.constants;
+
+public class Constants {
+
+    public static final String RECURSO_NAO_ENCONTRADO = "Recurso não encontrado!";
+    public static final String FALHA_INTEGRIDADE_REFERENCIAL = "Falha de integridade referencial!";
+}
